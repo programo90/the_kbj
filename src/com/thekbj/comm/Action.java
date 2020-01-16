1347634c1ab1619d4128714b90d82b6fe4376a8d
bit <leekyoungminimi@gmail.com>
2020-01-16 21:09:22 +0900

    Action.java_lkm_commit

---
+++ src/com/thekbj/comm/Action.java
@@ -0,0 +1,11 @@
+package com.thekbj.comm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+	public ForwardAction execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
