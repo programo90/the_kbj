1d29e62cce845c2059344676d5238cc2b21b48c5
bitcamp <lbo1989@naver.com>
2020-01-17 11:07:12 +0900

    서비스 패키지 하나로 통합

--- src/com/thekbj/member/MemberController.java
+++ src/com/thekbj/member/MemberController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.thekbj.member.service.MemberService;
+import com.thekbj.service.MemberService;
 
 /**
  * Servlet implementation class MemberController
