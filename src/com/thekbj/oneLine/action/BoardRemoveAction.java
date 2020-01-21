30cb0d6f49a0c4e122e49d9ae023c270869ce307
SuperCom <SuperCom@DESKTOP-M97L5UF>
2020-01-21 22:57:06 +0900

    oneLine remove js,action생성!

---
+++ src/com/thekbj/oneLine/action/BoardRemoveAction.java
@@ -0,0 +1,32 @@
+package com.thekbj.oneLine.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.thekbj.comm.Action;
+import com.thekbj.comm.ForwardAction;
+import com.thekbj.service.OneLineService;
+
+public class BoardRemoveAction implements Action {
+
+	@Override
+	public ForwardAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("BoardRemoveAction load");
+		
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		
+		OneLineService service = OneLineService.getService();
+		//int result = service.boardRemove(bno);
+		
+		ForwardAction forward = new ForwardAction();
+		forward.setForward(true);
+		forward.setUrl("/WEB-INF/oneLine/list.jsp");
+		 
+		return forward;
+	}
+
+}
